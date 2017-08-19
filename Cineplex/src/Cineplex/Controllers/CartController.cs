b67585162ed81fbdf7b86443e459617f88772ed9
Mattw46 <mattw76@live.com.au>
2017-08-19 13:47:02 +1000

    Added Cart and checkout controllers + views

---
+++ Cineplex/src/Cineplex/Controllers/CartController.cs
@@ -0,0 +1,155 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cineplex.Models;
+
+namespace Cineplex.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CineplexContext _context;
+
+        public CartController(CineplexContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Cart
+        public async Task<IActionResult> Index()
+        {
+            var cineplexContext = _context.Booking.Include(b => b.Session);
+            return View(await cineplexContext.ToListAsync());
+        }
+
+        // GET: Cart/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Booking.SingleOrDefaultAsync(m => m.BookingId == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // GET: Cart/Create
+        public IActionResult Create()
+        {
+            ViewData["SessionId"] = new SelectList(_context.Session, "SessionId", "SessionId");
+            return View();
+        }
+
+        // POST: Cart/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("BookingId,BookingType,SessionId")] Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(booking);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewData["SessionId"] = new SelectList(_context.Session, "SessionId", "SessionId", booking.SessionId);
+            return View(booking);
+        }
+
+        // GET: Cart/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Booking.SingleOrDefaultAsync(m => m.BookingId == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+            ViewData["SessionId"] = new SelectList(_context.Session, "SessionId", "SessionId", booking.SessionId);
+            return View(booking);
+        }
+
+        // POST: Cart/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("BookingId,BookingType,SessionId")] Booking booking)
+        {
+            if (id != booking.BookingId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(booking);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookingExists(booking.BookingId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            ViewData["SessionId"] = new SelectList(_context.Session, "SessionId", "SessionId", booking.SessionId);
+            return View(booking);
+        }
+
+        // GET: Cart/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Booking.SingleOrDefaultAsync(m => m.BookingId == id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // POST: Cart/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var booking = await _context.Booking.SingleOrDefaultAsync(m => m.BookingId == id);
+            _context.Booking.Remove(booking);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool BookingExists(int id)
+        {
+            return _context.Booking.Any(e => e.BookingId == id);
+        }
+    }
+}
