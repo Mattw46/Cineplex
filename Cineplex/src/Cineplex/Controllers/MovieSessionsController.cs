d342af89dde4fb8f565534ecb6ed4e608dee646c
Mattw46 <mattw76@live.com.au>
2017-08-19 16:15:53 +1000

    Implemented Movie Session search

--- Cineplex/src/Cineplex/Controllers/MovieSessionsController.cs
+++ Cineplex/src/Cineplex/Controllers/MovieSessionsController.cs
@@ -18,14 +18,44 @@ namespace Cineplex.Controllers
             _context = context;    
         }
 
-        // GET: MovieSessions
-        public async Task<IActionResult> Index()
+        // GET: MovieSessions filtered
+        public async Task<IActionResult> Index(string cinemaString, string movieString)
         {
-            /*var cineplexContext = _context.Session.Include(s => s.Cinema).Include(s => s.Movie);
-            return View(await cineplexContext.ToListAsync());*/
-
-            var cineplexContext = _context.Session.Include(s => s.Cinema).Include(s => s.Movie).AsNoTracking();
-            return View(await cineplexContext.ToListAsync());
+            /* I encountered an issue here where I could not declare var session up front
+               without assigning a result set. this meant that session was out of scope
+               when returning at the end of the method. i had no choice but to duplicate the
+               return statement inside each if statement */
+            
+            if (!String.IsNullOrEmpty(cinemaString) && String.IsNullOrEmpty(movieString))
+            {
+                var sessions = _context.Session.Include(s => s.Cinema)
+                    .Include(s => s.Movie).AsNoTracking()
+                    .Where(s => s.Cinema.Location.Contains(cinemaString));
+                return View(await sessions.ToListAsync());
+            }
+            else if (String.IsNullOrEmpty(cinemaString) && !String.IsNullOrEmpty(movieString))
+            {
+                var sessions = _context.Session.Include(s => s.Cinema)
+                    .Include(s => s.Movie).AsNoTracking()
+                    .Where(s => s.Movie.Title.Contains(movieString));
+                return View(await sessions.ToListAsync());
+            }
+            else if (!String.IsNullOrEmpty(cinemaString) && !String.IsNullOrEmpty(movieString))
+            {
+                var sessions = _context.Session.Include(s => s.Cinema)
+                    .Include(s => s.Movie)
+                    .Where(s => s.Cinema.Location.Contains(cinemaString))
+                    .Where(s => s.Movie.Title.Contains(movieString)).AsNoTracking();
+                return View(await sessions.ToListAsync());
+            }
+            else
+            {
+                var sessions = _context.Session.Include(s => s.Cinema)
+                    .Include(s => s.Movie).AsNoTracking();
+                return View(await sessions.ToListAsync());
+            }
+            
+            
         }
 
         // GET: MovieSessions/Details/5
