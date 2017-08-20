8140db8b7e7a507933a55275f33dacf6e4c8d59a
Mattw46 <mattw76@live.com.au>
2017-08-20 19:48:26 +1000

    added basic cart

---
+++ Cineplex/src/Cineplex/Models/CartItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cineplex.Models
+{
+    public class CartItem : System.Collections.IEnumerable
+    {
+        public Session session { get; set; }
+        public int quantity { get; set; }
+        //public ArrayList seats { get; set; }
+
+        public CartItem()
+        {
+            //seats = new ArrayList();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        /*public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }*/
+    }
+}
